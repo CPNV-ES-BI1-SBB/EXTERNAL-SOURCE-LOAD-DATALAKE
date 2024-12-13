44f34fba36d8cbd58b40072f1e3b0192cb9aa63f
JuilletMikael <mikael.juillet@eduvaud.ch>
2024-12-13 15:35:53 +0100

    feat: implement server with fastapi

--- src/Server.py
+++ src/Server.py
@@ -1,22 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from src.cloud_services.cloud_service import CloudService
+from src.objects.json_object import JsonObject
 
 
 class Server:
-    _host : str
-    _port : int
     _app : FastAPI
     _service : CloudService
 
-    def __init__(self, host : str, port : int):
-        pass
+    def __init__(self, service : CloudService):
+        self._service = service
+        self._app = FastAPI()
 
-    def start(self, service : CloudService):
-        pass
+    def start(self):
+        self._service.connect()
+
+        @self._app.post("/load")
+        async def load_content(request: Request):
+            # TODO : implement multy types with object types
+            self._service.load(await request.json())
+
 
     def stop(self):
-        pass
-
-    def load(self, content : any):
-        pass
+        self._service.disconnect()
